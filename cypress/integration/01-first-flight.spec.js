e5963c6e8e18d615cd093b71f40caf9ff2bd0b09
JeremyXZ <jeremyuk168@gmail.com>
2023-07-31 23:45:23 +0100

    done 01-first-flight file

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+  beforeEach(() => {
+    cy.visit('/jetsetter');
+  });
+  //cy.get will return a collection of specific element if the chained commands suggest its' a collection, eg. "should("have.length", 3)
+  // it can return the first element of the collection if the chained commands suggest it's a single element, eg. ".click()", ".type()"
+  it('should have a form', () => {
+    cy.get('form').should('exist');
+  });
+  it('should have the word "Add Item', () => {
+    cy.contains('Add Item');
+  });
+
+  it('should put stuff in the input field', () => {
+    cy.get('[data-test="new-item-input"]').type('great stuff');
+  });
+});
