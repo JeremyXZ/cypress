c979ea97ef095944a95f10b8f78e730eb0ab54c9
JeremyXZ <jeremyuk168@gmail.com>
2023-08-09 16:40:55 +0100

    done ex11 and ex12

--- cypress/integration/11-pokemon-search.spec.js
+++ cypress/integration/11-pokemon-search.spec.js
@@ -6,31 +6,110 @@ const pokemon = [
   { id: 3, name: 'Turtle' },
 ];
 
+//define getElement custom command:
+Cypress.Commands.add('getEl', (selector) => {
+  cy.get(`[data-test="${selector}"]`);
+});
+
 describe('Pokémon Search', () => {
   beforeEach(() => {
     cy.visit('/pokemon-search');
 
-    cy.get('[data-test="search"]').as('search');
-    cy.get('[data-test="search-label"]').as('label');
+    cy.getEl('search').as('searchInput');
+    cy.getEl('search-label').as('searchLabel');
 
     cy.intercept('/pokemon-search/api?*').as('api');
   });
 
-  it('should call the API when the user types', () => {});
+  it('should call the API when the user types', () => {
+    cy.get('@searchInput').type('Ivysaur');
+    cy.wait('@api');
+  });
 
-  it('should update the query parameter', () => {});
+  it('should update the query parameter', () => {
+    cy.get('@searchInput').type('Ivy');
+    cy.wait('@api');
 
-  it('should call the API with correct query parameter', () => {});
+    //checks for an exact match of the query string including "?"
+    cy.location('search').should('equal', '?name=Ivy');
+    //checks for the presence of a substring within the query string (there can be other substrings co-existing)
+    cy.location('search').should('contain', 'name=Ivy');
+  });
 
-  it('should pre-populate the search field with the query parameter', () => {});
+  //The interception object contains info about the intercepted request (e.g. method, URL, headers)
+  //and response including statusCode, body, header.
+  it('should make an Ajax request correcly (as expected)', () => {
+    cy.get('@searchInput').type('Turtle');
+    cy.wait('@api').then((interception) => {
+      expect(interception.request.url).to.contain('name=Turtle');
+    });
+    // alternatively using .its() to access properties on the interception object and make assertions.
+    // cy.wait('@api').its('request.url').should('contain', 'name=Turtle');
+  });
 
-  it('should render the results to the page', () => {});
+  //qs means query string, checking the query parameter corresponding to the input field
+  it('should pre-populate the search field with the query parameter', () => {
+    cy.visit({ url: '/pokemon-search', qs: { name: 'char' } });
+    cy.get('@searchInput').should('have.value', 'char');
+  });
 
-  it('should link to the correct pokémon', () => {});
+  it('should render the results to the page', () => {
+    // intercepted (stub) a request with any query string ("*" after ?) in the url and sent the pokemon object (defined at top) as the response
+    //so request didn't rearch the actual server
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
 
-  it('should persist the query parameter in the link to a pokémon', () => {});
+    //no matter what the query string is (name="happy coding" as below), it won't affect the pokemon object being shown on the page
+    //because the server didn't have the chance to handle the query string as the request was intercepted and responded with the mocked data "pokemon"
+    cy.get('@searchInput').type('happy coding');
 
-  it('should bring you to the route for the correct pokémon', () => {});
+    cy.wait('@stubbed-api');
 
-  it('should immediately fetch a pokémon if a query parameter is provided', () => {});
+    cy.get('[data-test="result"]').should('have.length', 3);
+    cy.get('[data-test="result"]').should('contain', 'Charmer');
+  });
+
+  it('should link to the correct pokémon', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    cy.get('@searchInput').type('anything');
+    cy.wait('@stubbed-api');
+
+    cy.get('[data-test="result"] a').each(($el, index) => {
+      const { id } = pokemon[index];
+      cy.wrap($el).should('have.attr', 'href').and('contain', `/pokemon-search/${id}`);
+    });
+  });
+
+  it('should persist the query parameter in the link to a pokémon', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+
+    cy.get('@searchInput').type('happy');
+    cy.wait('@stubbed-api');
+
+    cy.get('[data-test="result"] a').each(($el) => {
+      cy.wrap($el).should('have.attr', 'href').and('contain', 'name=happy');
+      // expect($el.attr('href')).to.contain('name=happy');
+    });
+  });
+
+  //typing into the search field trigger api calls
+  it('should check if typing into the search box and clicking links can trigger two separate api calls and can display expected results respectively', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    cy.intercept('/pokemon-search/api/1', { fixture: 'bulbasaur.json' }).as('individual-api');
+
+    cy.get('@searchInput').type('bulba');
+    cy.wait('@stubbed-api');
+
+    cy.get('[data-test="result"] a').first().click();
+    cy.wait('@individual-api');
+
+    cy.location('pathname').should('contain', '/pokemon-search/1');
+  });
+
+  it('should make an api call with the correct query string if a user visits the page with a specific query parameter in it', () => {
+    cy.intercept('/pokemon-search/api?*', { pokemon }).as('stubbed-api');
+    cy.visit({ url: '/pokemon-search', qs: { name: 'bulba' } });
+
+    //check the response object's property ("url") inside the api call
+    cy.wait('@stubbed-api').its('response.url').should('contain', '?name=bulba');
+  });
 });
--- cypress/integration/12-dog-facts.spec.js
+++ cypress/integration/12-dog-facts.spec.js
@@ -13,16 +13,46 @@ describe('Dog Facts', () => {
   });
 
   it('should start out with an empty state', () => {
-    cy.get('@emptyState');
+    cy.get('@emptyState').should('be.visible');
   });
 
-  it('should make a request when the button is called', () => {});
+  it('should no longer have an empty state after a fetch', () => {
+    cy.get('@fetchButton').click();
+    cy.should('not.contain', 'Fetch some dog facts or something.');
+  });
 
-  it('should adjust the amount when the select is changed', () => {});
+  it('should make a request when the button is called', () => {
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+    cy.get('#facts p').should('have.length', 3);
+  });
 
-  it('should show the correct number of facts on the page', () => {});
+  it('should adjust the amount when the select is changed', () => {
+    cy.get('@amountSelect').select('7');
+    cy.get('@fetchButton').click();
+    cy.wait('@api').then((interception) => {
+      cy.wrap(interception.request.url).should('contain', 'amount=7');
+      // expect(interception.request.url).to.match(/\?amount=4$/);
+    });
+  });
 
-  it('should clear the facts when the "Clear" button is pressed', () => {});
+  // cy.wait can be omitted since Cypress will have 4 seconds auto-wait by default (runing the query again and again until 4 seconds run out)
+  //which is good for asynchronous operations
+  it('should show the correct number of facts on the page', () => {
+    cy.get('@amountSelect').select('5 Facts');
+    cy.get('@fetchButton').click();
+    cy.wait('@api');
+    cy.get('#facts p').should('have.length', 5);
+  });
 
-  it("should reflect the number of facts we're looking for in the title", () => {});
+  it('should clear the facts when the "Clear" button is pressed', () => {
+    cy.get('@clearButton').click();
+    cy.get('@emptyState').should('exist');
+  });
+
+  it.only("should reflect the number of facts we're looking for in the title", () => {
+    cy.title().should('equal', '3 Dog Facts');
+    cy.get('@amountSelect').select('10');
+    cy.title().should('equal', '10 Dog Facts');
+  });
 });
